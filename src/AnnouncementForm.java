10625c305714ffad5727495730cf00d3fdc0c2ab
vuvanlinhk3 <vanlinh30122003@gmail.com>
2023-11-23 21:46:30 +0700

    them form thong bao

--- src/AnnouncementForm.java
+++ src/AnnouncementForm.java
@@ -1,2 +1,19 @@
-public class AnnouncementForm {
+import javax.swing.*;
+import java.awt.*;
+
+public class AnnouncementForm extends JFrame {
+    public AnnouncementForm(){
+        setTitle("Thông Báo");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(700,500);
+        setLocationRelativeTo(null);
+        setVisible(true);
+//        setLocation(300,300);
+        FlowLayout flowLayout = new FlowLayout();
+        setLayout(flowLayout);
+    }
+    public static void main(String[] args) {
+        new AnnouncementForm();
+
+    }
 }
