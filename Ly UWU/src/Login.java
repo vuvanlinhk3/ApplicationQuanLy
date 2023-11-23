7ffb623f96fcd6fdcc1cefa9fe01574abfbf95d2
Kieranguyen <151450697+Kieranguyen@users.noreply.github.com>
2023-11-23 22:46:08 +0700

    uwu

---
+++ Ly UWU/src/Login.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Login {
+    private JTextArea mootConVitTextArea;
+    private JPanel panel1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
---
+++ Ly UWU/src/dangki.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class dangki {
+    private JTextPane textPane1;
+}
