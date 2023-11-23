2472971fffaa4c45545561b3436515c1ca9edf4b
livaan <126743998+vuvanlinhk3@users.noreply.github.com>
2023-11-23 23:00:24 +0700

    Delete Ly UWU directory

--- Ly UWU/src/buoi_toi_tot_lanh.java
+++
@@ -1,7 +0,0 @@
-public class buoi_toi_tot_lanh {
-
-    public static void main (String[] args){
-        System.out.println(" UWU ");
-    }
-
-}
--- Ly UWU/src/dangki.java
+++
@@ -1,5 +0,0 @@
-import javax.swing.*;
-
-public class dangki {
-    private JTextPane textPane1;
-}
