c3e27538ea7fbbacd5800b3d24ec77fbc0643ecd
vuvanlinhk3 <vanlinh30122003@gmail.com>
2023-11-25 20:16:24 +0700

    add form room config

---
+++ src/RoomConfig.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.tree.RowMapper;
+
+public class RoomConfig extends JFrame {
+    public RoomConfig() {
+
+    }
+
+    public static void main(String[] args) {
+        new RoomConfig();
+    }
+}
