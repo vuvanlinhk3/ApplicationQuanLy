f83bd025e3a80f9881ae106369908f473d646b41
vuvanlinhk3 <vanlinh30122003@gmail.com>
2023-11-26 21:43:56 +0700

    change not many :>

--- src/main/java/Announcement/AnnouncementApplication.java
+++ src/main/java/Announcement/AnnouncementApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class AnnouncementApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(AnnouncementApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(AnnouncementApplication.class.getResource("AnnouncementForm.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1600, 900);
         stage.setTitle("Thông Báo !");
         stage.setScene(scene);
