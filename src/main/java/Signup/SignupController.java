41d8b1090adb33be7112e2187b30437634307b6e
Kieranguyen <151450697+Kieranguyen@users.noreply.github.com>
2023-12-01 00:41:13 +0700

    giao dien dang nhap

---
+++ src/main/java/Signup/SignupController.java
@@ -0,0 +1,16 @@
+package Signup;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SignupController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
