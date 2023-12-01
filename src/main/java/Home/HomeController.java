dd30c69ee089f1d08eef06db77e2c51a4bddabe3
Peas <hoanganhpeas003@gmail.com>
2023-12-02 01:56:39 +0700

    HomeForm

---
+++ src/main/java/Home/HomeController.java
@@ -0,0 +1,15 @@
+package Home;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class HomeController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
